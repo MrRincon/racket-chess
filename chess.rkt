3997b78ff6fe03b0f5e47e9f68d1223cb372169b
Mi1kDev <tjthemorgan@gmail.com>
2023-03-15 08:03:07 +0000

    Working chess piece movement

--- chess.rkt
+++ chess.rkt
@@ -1,46 +1,73 @@
+#|Instructions: 
+    Left Click to Select and Right Click to Place
+    Assumptions Made: Placing on another sprite implies a capture
+|#
+
 #lang racket/gui
+;import the necessary packages
 (require 2htdp/image (only-in pict pict->bitmap))
 (require "chessstate.rkt")
-(define imgFolder (path->string (build-path (current-directory) "racket-chess\\sprites")))
 
+;define a path to the image folder in which sprites are contained for easy use later
+(define imgFolder (path->string (build-path (current-directory) "racket-chess\\sprites")))
+;we create a frame
 (define root (new frame% [label "Chess"][width 194][height 218]))
 
+;converts an image object from 2htdp/image to a bitmap
 (define img->bmap (lambda (image)
     (pict->bitmap image)
 ))
-
+;uses provide coordinates to place a chess piece on the board
 (define placePiece (lambda (board cPiece coords)
     (overlay/offset cPiece (first coords) (second coords) board)
 ))
-
+;gets the bitmap of an image from a file, in this case we need the sprites from the image folder
 (define getBitmap (lambda (filename)
     (bitmap/file (string-append imgFolder "\\" filename))
 ))
-
-
-
+;loops through a given boardstate and places all chess pieces in the correct position
 (define drawBoard (lambda (boardState iBoardState)
     (cond
         [(empty? boardState) (img->bmap iBoardState)]
         [#t (drawBoard (rest boardState) (placePiece iBoardState (getBitmap (piece-sprite (piecePos-piece (first boardState)))) (send board translatePiecePosToBoardPos (first boardState))))]
     )
 ))
-
+;an extension of the canvas class. Additional functions are added and some overriden
 (define ccanvas% (class canvas%
     (super-new)
+    ;we keep track of both the starting position or the position of the piece we are going to move as well as the ending position or where we move the piece to
     [init-field (startPos '())]
     [init-field (endPos '())]
-    (define/override (on-e
-    vent event)
+    ;left mouse button is used to select a piece and right to place the selected piece
+    (define/override (on-event event)
+        ;when the left mouse button is pressed and the cursor is within the bounds of the board, we set the startingPos to the column and row returned
         (when (send event button-down? 'left)
             (let ((click (clickHandler (send event get-x) (send event get-y))))
                 (cond
                     [(equal? click #f)]
+                    [#t (set! startPos click)]
                 )
             )
-            
         )
+        ;when the right mpise button is pressed and the cursor is within the bounds of the board, we set the endPos to the column and row returned
+        (when (send event button-down? 'right)
+            (let ((click (clickHandler (send event get-x) (send event get-y))))
+                (cond
+                    [(equal? click #f)]
+                    [#t (set! endPos click)]
+                )
+                ;if there is a start and end position then we move the pieces using these two positions and then redraw the board to display the change
+                (cond
+                    [(not(and (equal? endPos '()) (equal? startPos '())))
+                        (send board makeMove (car startPos) (cdr startPos) (car endPos) (cdr endPos))
+                        (update)
+                    ]
+                )
+            )
+        )
+        
     )
+    ;determines which row or column the cursor was in given its x and y coordinates
     (define/private clickHandler (lambda (x y)
         (let ((coordX "") (coordY "") (finalCoords ""))
             (cond
@@ -68,17 +95,18 @@
             )
         )
     ))
+    ;redraws the board
+    (define/private update (lambda ()
+        (send (send this get-dc) draw-bitmap (drawBoard (send board getBoard) (getBitmap "board.png")) 0 0)
+    ))
 ))
 
+;initialize the canvas and draw the board for the first time during its paint-callback
 (new ccanvas%
     [parent root]
-)
-#|
-(new canvas% 
-    [parent  root]
     [paint-callback (lambda (canvas dc)
         (send dc draw-bitmap (drawBoard (send board getBoard) (getBitmap "board.png")) 0 0)
-    )]    
-)|#
+    )]
+)
 
 (send root show #t)
--- chessstate.rkt
+++ chessstate.rkt
@@ -1,7 +1,9 @@
 #lang racket
-
+;data structure containing the information of a single piece. The name, color and sprite to display
 (struct piece (name color sprite))
+;an extension of the previous data structure which holds the piece's position on the board in terms of columns and rows
 (struct piecePos (piece column row))
+;the following are representations of the various types of pieces in chess for easy use later on
 (define whiteRook(piece "rook" "white" "white_rook.png"))
 (define blackRook (piece "rook" "black" "black_rook.png"))
 (define whiteKnight (piece "knight" "white" "white_knight.png"))
@@ -15,8 +17,11 @@
 (define whitePawn (piece "pawn" "white" "white_pawn.png"))
 (define blackPawn (piece "pawn" "black" "black_pawn.png"))
 
+;This class contains the current setup of the chessboard and the means to change the state of the board through movement of pieces
 (define chess% (class object%
     (super-new)
+    ;The boardstate is the starting position of the board and any changes are made assuming this as said starting point
+    ;It is a list containing the position of a typical initial chessboard setup
     [init-field (boardState (list
         (piecePos whiteRook "A" "1")
         (piecePos whiteKnight "B" "1")
@@ -52,7 +57,7 @@
         (piecePos blackKnight "G" "8")
         (piecePos blackRook "H" "8")
     ))]
-
+    ;The cells field is used to convert various rows and columns to their pixel by pixel counterpart when overlayed onto the chessboard
     [init-field (cells (hash 
         "A" 77
         "B" 55
@@ -72,7 +77,7 @@
         "8" 76
     ))]
 
-
+    ;Determines whether a given piece "checkPiece" exists in the board at the specified col and row
     (define/private checkPieceAtPos (lambda (checkPiece board col row)
         (cond
             [(empty? board) #f]
@@ -80,17 +85,27 @@
             [#t (checkPieceAtPos checkPiece (rest board) col row)]
         )
     ))
+    ;Returns the piece at a given col and row and if there exists no piece then returns #f
     (define/private getPieceAtPos (lambda (board col row)
         (cond
             [(empty? board) #f]
             [(and (equal? (piecePos-column (first board)) col) (equal? (piecePos-row (first board)) row)) (first board)]
-            [#t (getPieceAtPos (rest board) col procedure-reduce-keyword-arity-mask)]
+            [#t (getPieceAtPos (rest board) col row)]
         )
     ))
+    ;Updates the board to reflect any changes made
     (define/private updateBoard (lambda (newBoard)
         (set! boardState newBoard)
     ))
-
+    ;Similar to checkPieceAtPos but instead of requiring and checking that a specific piece exists, this checks if any piece exists at col and row
+    (define/private existsAtPos (lambda (board col row)
+        (cond
+            [(empty? board) #f]
+            [(and (equal? (piecePos-column (first board)) col) (equal? (piecePos-row (first board)) row)) #t]
+            [#t (existsAtPos (rest board) col row)]
+        )
+    ))
+    ;converts the column and row information of a piece into the appropriate pixel coordinates
     (define/public translatePiecePosToBoardPos (lambda (cPiece)
         (list (hash-ref cells (piecePos-column cPiece)) (hash-ref cells (piecePos-row cPiece)))
     ))
@@ -98,14 +113,25 @@
     (define/public returnPosPx (lambda (x y)
         (list (hash-ref cells x) (hash-ref cells y))
     ))
-
+    ;returns the current state of the board
     (define/public getBoard(lambda ()
         boardState
     ))
-
-    (define/public makeMove (lambda (cPiece icolumn irow column row)
-        (println(checkPieceAtPos cPiece boardState icolumn irow))
-        (println(getPieceAtPos boardState column row))
+    ;moves whatever piece (if any) is found at icolumn irow to column row
+    ;if another piece exists at column row then it is deleted. This is assumed to be a capture
+    (define/public makeMove (lambda (icolumn irow column row)
+        (cond
+            [(existsAtPos boardState icolumn irow)
+                (let ((foundFirstPiece (getPieceAtPos boardState icolumn irow)) (foundNextPiece (getPieceAtPos boardState column row)))
+                    (cond
+                        [(equal? foundNextPiece #f)
+                            (updateBoard (append (remove foundFirstPiece boardState) (list (piecePos (piecePos-piece foundFirstPiece) column row))))
+                        ]
+                        [#t (updateBoard (append (remove foundNextPiece (remove foundFirstPiece boardState)) (list (piecePos (piecePos-piece foundFirstPiece) column row))))]
+                    )
+                )
+            ]
+        )
     ))
 ))
 
